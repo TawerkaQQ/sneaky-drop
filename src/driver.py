73f1cc31413848563707fb3e890397717d10bb3d
Gekcer <gekcer@gmail.com>
2024-07-16 16:48:13 +0300

    -> добавить использование прокси

--- src/driver.py
+++ src/driver.py
@@ -1,2 +1,15 @@
 from selenium import webdriver
 
+def get_chrome_driver(use_proxy=False, use_user_agent=False):
+    chrome_options = webdriver.ChromeOptions()
+
+    if use_proxy:
+        pass
+
+    if use_user_agent:
+        pass
+
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
