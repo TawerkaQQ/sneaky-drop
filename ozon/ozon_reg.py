70037e47e394f6e3a502cd4633efa1a8ae1ef0d4
Gekcer <gekcer@gmail.com>
2024-07-29 10:48:09 +0300

    используем джаваскрипт + следим за появлением тега iframe

--- ozon/ozon_reg.py
+++ ozon/ozon_reg.py
@@ -10,7 +10,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from faker import Faker
+from bs4 import BeautifulSoup
 
 # from pass_generator import generator as gen
 from driver import get_chrome_driver
@@ -20,29 +22,52 @@ class Ozon_reg():
     def __init__(self):
         self.driver = get_chrome_driver(use_proxy=True, use_user_agent=False)
         self.url = 'https://www.ozon.ru/'
+        self.wait = WebDriverWait(self.driver, 20)
 
     def ozon_reg(self):
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        time.sleep(4)
+        come_in = self.find_come_in_element()
 
-        try:
-            wait = WebDriverWait(self.driver, 10)
-            self.driver.get(self.url)
-            time.sleep(7)
-            #
-            come_in = self.driver.find_element(By.CLASS_NAME, "d409-a")
+    def find_come_in_element(self):
+        element = self.driver.execute_script("""
+            var spanElements = document.evaluate("//span[contains(text(), 'Войти')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            return spanElements.singleNodeValue;
+        """)
+        if not element:
+            print('no')
+            time.sleep(400)
+        else:
+            time.sleep(1)
+            actions = ActionChains(self.driver)
+            actions.move_to_element(element).perform()
+            time.sleep(3)
+            element.click()
             time.sleep(2)
-            come_in.click()
-            time.sleep(4)
+            iframe = self.driver.execute_script("""
+                var inputElement = document.evaluate("//iframe[@id='authFrame']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                return inputElement.singleNodeValue;
+            """)
+            self.driver.switch_to.frame(iframe)
+            element = self.driver.execute_script("""
+                            var inputElement = document.evaluate("//input", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                            return inputElement.singleNodeValue;
+                        """)
+            time.sleep(10)
+            element.send_keys('11111')
+            time.sleep(10000)
 
-            tel_input = self.driver.find_element(By.NAME, "autocomplete")
-            tel_input.send_keys('987')
-            time.sleep(1000)
+    def find_come_in_element_bs4(self):
+        html = self.driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        res = soup.find('svg', {'width': '24', 'height': '24'})
+        print(len(res))
 
-        except:
-            come_in = self.driver.find_element(By.CLASS_NAME, "b6012-a3")
-            time.sleep(2)
-            come_in.click()
-            time.sleep(1000)
-            pass
+    def connect_to_ozon(self):
+        self.driver.get(self.url)
+        time.sleep(3)
+        return self
 
 
 if __name__ == '__main__':
