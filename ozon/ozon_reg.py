806a30edc0499a2e562b28b87bab7b65a3dd3078
Ivan <ivkalinin@sfedu.ru>
2024-07-29 11:16:58 +0300

    clear config + ozon_reg

--- ozon/ozon_reg.py
+++ ozon/ozon_reg.py
@@ -16,16 +16,15 @@ from bs4 import BeautifulSoup
 
 # from pass_generator import generator as gen
 from driver import get_chrome_driver
-from config.url_config import GOOGLE_MAIL_REGISTER_URL
+from config.url_config import GOOGLE_MAIL_REGISTER_URL, OZON_URL
 
-class Ozon_reg():
+class OzonReg():
     def __init__(self):
         self.driver = get_chrome_driver(use_proxy=True, use_user_agent=False)
-        self.url = 'https://www.ozon.ru/'
         self.wait = WebDriverWait(self.driver, 20)
 
     def ozon_reg(self):
-        self.driver.get(self.url)
+        self.driver.get(OZON_URL)
         self.driver.maximize_window()
         time.sleep(4)
         come_in = self.find_come_in_element()
@@ -39,9 +38,6 @@ class Ozon_reg():
             print('no')
             time.sleep(400)
         else:
-            time.sleep(1)
-            actions = ActionChains(self.driver)
-            actions.move_to_element(element).perform()
             time.sleep(3)
             element.click()
             time.sleep(2)
@@ -54,23 +50,13 @@ class Ozon_reg():
                             var inputElement = document.evaluate("//input", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
                             return inputElement.singleNodeValue;
                         """)
-            time.sleep(10)
+            time.sleep(20)
             element.send_keys('11111')
             time.sleep(10000)
 
-    def find_come_in_element_bs4(self):
-        html = self.driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        res = soup.find('svg', {'width': '24', 'height': '24'})
-        print(len(res))
-
-    def connect_to_ozon(self):
-        self.driver.get(self.url)
-        time.sleep(3)
-        return self
 
 
 if __name__ == '__main__':
 
-    sel_ozon = Ozon_reg()
+    sel_ozon = OzonReg()
     sel_ozon.ozon_reg()
