7b97f1f3b61f00b7d913da418ff765ddf04f583d
Gekcer <gekcer@gmail.com>
2024-08-02 20:12:58 +0300

    написал класс jsparser

--- ozon/ozon_reg.py
+++ ozon/ozon_reg.py
@@ -27,17 +27,15 @@ class OzonReg():
         self.driver.get(OZON_URL)
         self.driver.maximize_window()
         time.sleep(4)
-        reload_page = self.driver.find_element(By.XPATH, "//button[@class='rb']")
-        time.sleep(1)
-        reload_page.click()
+        # reload_page = self.driver.find_element(By.XPATH, "//button[@class='rb']")
+        # time.sleep(1)
+        # reload_page.click()
+        self.driver.refresh()
         time.sleep(15)
         come_in = self.find_come_in_element()
 
     def find_come_in_element(self):
-        element = self.driver.execute_script("""
-            var spanElements = document.evaluate("//span[contains(text(), 'Войти')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-            return spanElements.singleNodeValue;
-        """)
+        element = self.driver.execute_script( JSParser.search_element_script(tag='span', contain_text='Войти') )
         if not element:
             print('no')
             time.sleep(400)
@@ -45,35 +43,29 @@ class OzonReg():
             time.sleep(3)
             element.click()
             time.sleep(2)
-            iframe = self.driver.execute_script("""
-                var inputElement = document.evaluate("//iframe[@id='authFrame']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-                return inputElement.singleNodeValue;
-            """)
+            iframe = self.driver.execute_script( JSParser.search_element_script(tag='iframe', id='authFrame') )
             self.driver.switch_to.frame(iframe)
-            element = self.driver.execute_script("""
-                            var inputElement = document.evaluate("//input", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-                            return inputElement.singleNodeValue;
-                        """)
+            element = self.driver.execute_script( JSParser.search_element_script('input') )
             time.sleep(20)
             element.send_keys('999 694 46 82')
             time.sleep(4)
             # self.driver.switch_to.frame(iframe)
-
-            sign_in = self.driver.execute_script(
-                """
-                            var inputElement = document.evaluate("//button[@type='submit']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-                            return inputElement.singleNodeValue;
-                                    """
-                )
+            sign_in = self.driver.execute_script( JSParser.search_element_script(tag='button', add_params=True, type='submit') )
+            # sign_in = self.driver.execute_script("""
+            #                 var inputElement = document.evaluate("//button[@type='submit']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            #                 return inputElement.singleNodeValue;
+            #     """
+            #     )
             sign_in.click()
             time.sleep(4)
 
-            i_agree = self.driver.execute_script(
-                """
-                            var inputElement = document.evaluate("//input[@type='checkbox']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-                            return inputElement.singleNodeValue;
-                                    """
-            )
+            i_agree = self.driver.execute_script( JSParser.search_element_script(tag='input', add_params=True, type='checkbox') )
+            # i_agree = self.driver.execute_script(
+            #     """
+            #                 var inputElement = document.evaluate("//input[@type='checkbox']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            #                 return inputElement.singleNodeValue;
+            #                         """
+            # )
             time.sleep(4)
             i_agree.click()
 
@@ -87,6 +79,34 @@ class OzonReg():
             # get_new_code.click()
             time.sleep(4000)
 
+class JSParser:
+    @classmethod
+    def search_element_script(cls, tag, contain_text=None, add_params=None, **params):
+        if not contain_text:
+            if not add_params:
+                js_script = f"""
+                            var element = document.evaluate("//{tag}", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                            return element.singleNodeValue;
+                """
+            else:
+                print('yes')
+                tags_list = [f'@{x}' for x in params.keys()]
+                names_list = [f"'{x}'" for x in params.values()]
+                params_list = []
+                for x, y in zip(tags_list, names_list):
+                    params_list.append(f'{x}=')
+                    params_list.append(f'{y}, ')
+                params_string = ''.join(params_list)[:-2]
+                js_script = f"""
+                            var element = document.evaluate("//{tag}[{params_string}]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                            return element.singleNodeValue;
+                """
+        else:
+            js_script = f"""
+                        var element = document.evaluate("//{tag}[contains(text(), '{contain_text}')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                        return element.singleNodeValue;
+            """
+        return js_script
 
 if __name__ == '__main__':
 
